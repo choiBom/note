654c75037fab634c81074ca52ad5217b627f40c5
choiBom <bchoi@the-51.com>
2023-08-18 17:33:35 +0900

    아코디언 타입1 - 주석 추가

--- content/accordion/accordion.js
+++ content/accordion/accordion.js
@@ -48,16 +48,25 @@ const singleFolding = function (element, trigger) {
 };
 
 const syncFolding = function (element, trigger, targetRange) {
+    // element => useCollectAccordion() -> box -> '.c-accordion__box'
+    // trigger => useCollectAccordion() -> trigger -> // .c-accordion__body의 ID == 아코디언 버튼의 ID
+    // targetRange => useCollectAccordion() -> siblings ->  ['.js-accordion-handle-target']
     console.log("syncFolding")
     const status = element.classList.contains(selector.active);
     // all accordion target status reset to negative
     targetRange.forEach(t => {
+        // t => ['.js-accordion-handle-target']
+
+        // 아코디언 닫기
         t.classList.remove(selector.active);
+        // selector.active => .c-accordion__box--expand
         t.querySelector(selector.trigger).setAttribute('aria-expanded', false);
     });
     // set accordion box what clicked
     if (!status) {
+        // 아코디언 열기
         element.classList.add(selector.active);
+        // selector.active => .c-accordion__box--expand
         trigger.setAttribute('aria-expanded', true);
     }
 };
@@ -81,10 +90,18 @@ const allOf = function (element, trigger, siblings) {
 };
 
 const useCollectAccordion = function (el, siblings) {
-    // console.log("useCollectAccordion")
+    // accordion() -> el => .accordion-wrap03
+    // accordion() -> siblings => ['.js-accordion-handle-target']
+
     const contents = el.querySelector(selector.contents);
+    // selector.contents => '.c-accordion__body'
+
     const box = contents.closest(selector.box);
+    // selector.box => '.c-accordion__box'
+
     const trigger = el.querySelector(`[aria-controls="${contents.getAttribute('id')}"]`);
+    // .c-accordion__body의 ID == 아코디언 버튼의 ID
+
     const handler = function (event) {
         // // 1. filter event target
         // const correctButtonClick = trigger === event.target || event.target.closest(selector.trigger);
@@ -93,6 +110,7 @@ const useCollectAccordion = function (el, siblings) {
 
         // 2. execute
         switch (box.dataset.expand) {
+        // .c-accordion__box의 data-expand 의 값에 따라 케이스 적용
             // 2-3. active all under the {selector.target}
             case 'all':
                 return allOf(box, trigger, siblings);
@@ -102,6 +120,10 @@ const useCollectAccordion = function (el, siblings) {
             // 2-1. active sync other box
             default:
                 return syncFolding(box, trigger, siblings);
+                // .accordion-wrap03 > .c-accordion__box 에는 data-expand 없음 -> default 적용됨
+                // box => '.c-accordion__box'
+                // trigger => // .c-accordion__body의 ID == 아코디언 버튼의 ID
+                // siblings =>  ['.js-accordion-handle-target']
         }
     };
     return [trigger, handler];
@@ -113,13 +135,16 @@ const accordion = function (root) {
     if (beforeLaunch(element)) return false;
     const allAccordionHandleTarget = element.map(el => [...el.querySelectorAll(selector.handleTarget)]).flat(1); // returns an un-nested array.
 
-    // console.log(allAccordionHandleTarget);
     allAccordionHandleTarget.forEach((el, index, self) => {
+        console.log(el, "el")
+        console.log(self, "self")
         const parent = el.closest(selector.target);
         if (parent) {
             parent.classList.add(selector.initialized);
         }
         const [trigger, handler] = useCollectAccordion(el, self);
+        // el => .accordion-wrap03
+        // self => ['.js-accordion-handle-target']
         trigger.addEventListener('click', handler, false);
     });
 };
@@ -130,6 +155,7 @@ function init() {
     if (beforeLaunch(component)) return false;
     // console.log("init")
     component.forEach(el => accordion(el));
+    // el => .accordion-wrap03
 }
 
 init()
